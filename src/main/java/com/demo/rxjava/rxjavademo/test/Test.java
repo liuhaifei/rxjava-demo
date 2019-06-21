6ff61b7c0bc260b496b160f984c35f1e98fb6fee
liuhaifei <liuhaifei@enmonster.com>
2019-06-21 15:44:15 +0800

    rxjava-demo

---
+++ src/main/java/com/demo/rxjava/rxjavademo/test/Test.java
@@ -0,0 +1,14 @@
+package com.demo.rxjava.rxjavademo.test;
+
+/**
+ * @author liuhaifei
+ * @date 2019/6/21/021
+ * @Description:
+ * @Version 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("1111111111");
+    }
+}
