568e83b6cb20b18f8cf6b246e9d8b8300f35c552
liuhaifei <liuhaifei@enmonster.com>
2019-06-24 10:19:35 +0800

    test

--- src/main/java/com/demo/rxjava/rxjavademo/test/Test.java
+++ src/main/java/com/demo/rxjava/rxjavademo/test/Test.java
@@ -9,6 +9,6 @@ package com.demo.rxjava.rxjavademo.test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("1111111111");
+        System.out.println("222222222");
     }
 }
